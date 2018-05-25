09a17f52d4aca3630206dc79f2f7052a5e09745d
leizton <leizton@126.com>
2018-05-25 16:42:10 +0800

    TrieTreeTest

--- src/test/java/com/wh/test/util/TrieTreeTest.java
+++ src/test/java/com/wh/test/util/TrieTreeTest.java
@@ -15,22 +15,27 @@ public class TrieTreeTest {
     TrieTree.Node n1 = tree.strToNode("ab1");
     Assert.assertEquals(n1.getCh(), '1');
     Assert.assertEquals(n1.getLevel(), 3);
+    Assert.assertEquals("ab1", tree.nodeToStr(n1));
 
     TrieTree.Node n2 = tree.strToNode("ab1");
+    Assert.assertEquals("ab1", tree.nodeToStr(n2));
     Assert.assertSame(n1, n2);
     Assert.assertEquals(0, n1.compareTo(n2));
 
     TrieTree.Node n3 = tree.strToNode("ab2");
+    Assert.assertEquals("ab2", tree.nodeToStr(n3));
     Assert.assertNotSame(n1, n3);
     Assert.assertSame(n1.getParent(), n3.getParent());
     Assert.assertTrue(n1.compareTo(n3) < 0);
 
     TrieTree.Node n4 = tree.strToNode("ab2");
+    Assert.assertEquals("ab2", tree.nodeToStr(n4));
     Assert.assertSame(n3, n4);
     Assert.assertEquals(0, n4.compareTo(n3));
     Assert.assertTrue(n4.compareTo(n1) > 0);
 
     TrieTree.Node n5 = tree.strToNode("ac1");
+    Assert.assertEquals("ac1", tree.nodeToStr(n5));
     Assert.assertTrue(n5.compareTo(n1) > 0);
     Assert.assertTrue(n5.compareTo(n3) > 0);
     Assert.assertTrue(n1.compareTo(n5) < 0);
