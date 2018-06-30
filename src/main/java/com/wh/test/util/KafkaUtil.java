db4a1d6e2cb74d6231b929e6ebf31a3fd6b5b8ba
leizton <leizton@126.com>
2018-06-30 19:12:41 +0800

    catch consume exception

--- src/main/java/com/wh/test/util/KafkaUtil.java
+++ src/main/java/com/wh/test/util/KafkaUtil.java
@@ -7,6 +7,8 @@ import kafka.javaapi.consumer.ConsumerConnector;
 import kafka.javaapi.consumer.ZookeeperConsumerConnector;
 import kafka.message.MessageAndMetadata;
 import kafka.serializer.Decoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
@@ -19,24 +21,31 @@ import java.util.function.Consumer;
  */
 @SuppressWarnings("WeakerAccess")
 public class KafkaUtil {
+  private static final Logger LOG = LoggerFactory.getLogger(KafkaUtil.class);
 
   public static final Decoder<String> STRING_DECODER = (Decoder<String>) String::new;
 
   public static <K, V> void consume(String clusterZk, String topic, String consumerGroup, int parallelNum,
                                     Decoder<K> keyDecoder, Decoder<V> valueDecoder,
-                                    Consumer<MessageAndMetadata<K, V>> doConsume) {
+                                    Consumer<MessageAndMetadata<K, V>> consumer) {
     ConsumerConnector connector = ConsumerConnectorFactory.create(clusterZk, topic, consumerGroup);
     ExecutorService executor = Executors.newFixedThreadPool(parallelNum, new NamedThreadFactory("kafka-" + topic + "-" + consumerGroup));
-    consume(connector, topic, parallelNum, keyDecoder, valueDecoder, doConsume, executor);
+    consume(connector, topic, parallelNum, keyDecoder, valueDecoder, consumer, executor);
   }
 
   public static <K, V> void consume(ConsumerConnector connector, String topic, int parallelNum,
                                     Decoder<K> keyDecoder, Decoder<V> valueDecoder,
-                                    Consumer<MessageAndMetadata<K, V>> doConsume, ExecutorService executor) {
+                                    Consumer<MessageAndMetadata<K, V>> consumer, ExecutorService executor) {
     Map<String, List<KafkaStream<K, V>>> topicToStreams = connector.createMessageStreams(
         ImmutableMap.of(topic, parallelNum), keyDecoder, valueDecoder);
     List<KafkaStream<K, V>> kafkaStreams = topicToStreams.get(topic);
-    kafkaStreams.forEach(kafkaStream -> executor.submit(() -> kafkaStream.forEach(doConsume)));
+    kafkaStreams.forEach(kafkaStream -> executor.submit(() -> kafkaStream.forEach(msg -> {
+      try {
+        consumer.accept(msg);
+      } catch (Exception e) {
+        LOG.error("consume " + topic + " exception", e);
+      }
+    })));
   }
 
   private static final class ConsumerConnectorFactory {
