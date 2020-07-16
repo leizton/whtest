58ab0336ee08ffce1a8560ef76e4311fa95602ae
yaoqun.fyq <yaoqun.fyq@alibaba-inc.com>
2020-07-16 12:30:12 +0800

    mod

--- src/main/java/com/wh/test/util/CryptUtil.java
+++ src/main/java/com/wh/test/util/CryptUtil.java
@@ -1,8 +1,5 @@
 package com.wh.test.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
